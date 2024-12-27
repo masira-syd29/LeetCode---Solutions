9667a19aab0f1ffe2326799c152268ec61096463
masira-syd29 <maseera.sayed29@gmail.com>
2024-12-27 21:00:34 +0530

    Commit

---
+++ src/LongestSubstringWithRepeatingCharacters.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class LongestSubstringWithRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s){
+        HashSet<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int start = 0;
+        for(int end=0; end<s.length(); end++){
+            char currentChar = s.charAt(end);
+
+            while(set.contains(currentChar)){
+                set.remove(s.charAt(start));
+                start++;
+            }
+            set.add(currentChar);
+
+            maxLength = Math.max(maxLength, end-start+1);
+
+        }
+        return maxLength;
+    }
+}
